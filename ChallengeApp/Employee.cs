34664d557347a09d883502f06be19df38bcebbe3
Greta Pardyka <pardykag@gmail.com>
2023-10-02 12:36:37 +0200

    PracaDomowa 10

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -15,7 +15,43 @@
     
         public void AddGrade(float grade) 
         {
-            this.grades.Add(grade);
+            if (grade >=0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+
+        public void AddGrade(int grade)
+        {
+            float valueInInt = (float)grade;
+            this.AddGrade(valueInInt);
+        }
+
+        public void AddGrade(double grade)
+        {
+            float valueInDouble = (float)grade;
+            this.AddGrade(valueInDouble);
+        }
+
+        public void AddGrade(long grade)
+        {
+            float valueInLong = (float)grade;
+            this.AddGrade(valueInLong);
         }
 
         public Statistics GetStatistics() 
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -2,9 +2,11 @@
 
 var employee = new Employee("Jula", "Nowak");
 employee.AddGrade(2);
-employee.AddGrade(7);
-employee.AddGrade(4);
+employee.AddGrade("Kasia");
+employee.AddGrade(407);
+employee.AddGrade(5.89);
 var statistics = employee.GetStatistics();
+
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
