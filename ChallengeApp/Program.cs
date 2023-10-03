d10f4dbba522da8d3a09ba9f6a273803322edabd
Greta Pardyka <pardykag@gmail.com>
2023-10-03 12:37:15 +0200

    PracaDomowa11

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -6,8 +6,15 @@ employee.AddGrade("Kasia");
 employee.AddGrade(407);
 employee.AddGrade(5.89);
 var statistics = employee.GetStatistics();
+var statistics1 = employee.GetStatisticsWithForEach();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWithDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
 
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-// Console.WriteLine("Average: " + statistics.Max);
+Console.WriteLine($"Get Statistics With For Each: Average = {statistics1.Average:N2} , Min = {statistics1.Min} , MAx = {statistics1.Max}");
+
+Console.WriteLine($"Get Statistics With For: Average = {statistics2.Average:N2} , Min = {statistics2.Min} , Max = {statistics2.Max}");
+
+Console.WriteLine($"Get Statistics With Do While: Average = {statistics3.Average:N2} , Min = {statistics3.Min} , Max = {statistics3.Max}");
+
+Console.WriteLine($"Get Statistics With While: Average = {statistics4.Average:N2} , Min = {statistics4.Min} , Max = {statistics4.Max}");
