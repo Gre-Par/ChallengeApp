5be13d7849c2daed087f94e9e42abd16d3dff894
Greta Pardyka <pardykag@gmail.com>
2023-10-11 15:53:25 +0200

    PracaDomowa14

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -1,23 +1,14 @@
-﻿using static System.Formats.Asn1.AsnWriter;
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
-    public class Employee
+    public class Employee : Person
     {
         private List<float> grades = new List<float>();
-        public Employee()
-        {
-        }
-        public Employee(string name, string surname)
-        {
-            this.Name = name;
-            this.Surname = surname;
-        }
 
-        public string Name { get; private set; }
-
-        public string Surname { get; private set; }
-    
+        public Employee(string name, string surname, char sex)
+            : base(name, surname, sex)
+        {
+        
+        }
         public void AddGrade(float grade) 
         {
             if (grade >=0 && grade <= 100)
