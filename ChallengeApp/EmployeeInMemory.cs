260686d404fa63f5459734e6c663eabd77b9b4ff
Greta Pardyka <pardykag@gmail.com>
2023-10-19 18:58:02 +0200

    PracaDomowa16

---
+++ ChallengeApp/EmployeeInMemory.cs
@@ -0,0 +1,121 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInMemory : EmployeeBase
+    {
+        private List<float> grades = new List<float>();
+        public EmployeeInMemory(string name, string Surname) 
+            : base(name, Surname)
+        {
+        }
+        //public EmployeeInMemory(string name, string surname)
+        //{
+        //    this.Surname = surname;
+        //    this.Name = name;
+        //}
+        //public string Surname { get; private set; }
+        //public string Name { get; private set; }
+
+        public override void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public override void AddGrade(int grade)
+        {
+            float gradeAsFloat = grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public override void AddGrade(char grade)
+        {
+            switch (grade)
+            {
+                case 'A':
+                case 'a':
+                    AddGrade(100);
+                    break;
+                case 'B':
+                case 'b':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                case 'c':
+                    AddGrade(60);
+                    break;
+                case 'D':
+                case 'd':
+                    AddGrade(40);
+                    break;
+                case 'E':
+                case 'e':
+                    AddGrade(20);
+                    break;
+                default:
+                    throw new Exception("Wrong letter");
+            }
+        }
+
+        public override void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                throw new Exception("String is not float");
+            }
+        }
+
+        public override void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("invalid grade value");
+            }
+        }
+        public override Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                if (grade >= 0)
+                {
+                    statistics.Max = Math.Max(statistics.Max, grade);
+                    statistics.Min = Math.Min(statistics.Min, grade);
+                    statistics.Average += grade;
+                }
+            }
+
+            statistics.Average = statistics.Average / this.grades.Count;         // <=> statistics.Average /= this.grades.Count;
+            switch (statistics.Average)
+            {
+                case var average when average >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var average when average >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var average when average >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var average when average >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+            return statistics;
+        }
+    }
+}
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -4,11 +4,13 @@ Console.WriteLine("Witamy w programie do oceny pracowników, ocena kierownika");
 Console.WriteLine("=============================================");
 Console.WriteLine();
 
-var supervisor = new Supervisor() ;
+var employee = new EmployeeInFile("Greta", "Pardyka") ;
+//employee.AddGrade(0.5f);
+//employee.AddGrade(20);
 
 while (true)
 {
-    Console.WriteLine("Podaj kolejną ocenę kierownika: ");
+    Console.WriteLine("Podaj kolejną ocenę pracownika: ");
     var input = Console.ReadLine();
     if (input == "q")
     {
@@ -17,14 +19,14 @@ while (true)
 
     try
     {
-        supervisor.AddGrade(input);
+        employee.AddGrade(input);
     }
-    catch(Exception e)
+    catch (Exception e)
     {
         Console.WriteLine($"Exception catched: {e.Message}");
     }
 }
-var statistics = supervisor.GetStatistics();
+var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average}");
 Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Min: {statistics.Min}");
